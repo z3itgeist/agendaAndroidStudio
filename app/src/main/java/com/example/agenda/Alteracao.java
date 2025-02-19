4cc034a5862643f73d8ccbc28e3392ee22f3a396
z3itgeist <braga_gabriel@live.com>
2025-02-19 20:20:33 -0300

    adicionado tela de alteração e botões voltar

--- app/src/main/java/com/example/agenda/Alteracao.java
+++ app/src/main/java/com/example/agenda/Alteracao.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,29 +13,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Cadastro extends AppCompatActivity {
+public class Alteracao extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_cadastro);
+        setContentView(R.layout.activity_alteracao);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
-            EditText nome = findViewById(R.id.cNome);
-            EditText email = findViewById(R.id.cEmail);
-            EditText fone = findViewById(R.id.cFone);
-            ImageView btnSalvar = findViewById(R.id.cSalvar);
+            EditText nome = findViewById(R.id.aNome);
+            EditText email = findViewById(R.id.aEmail);
+            EditText fone = findViewById(R.id.aFone);
+            Button btnSalvar = findViewById(R.id.alterar);
 
-            Button btnVoltar = findViewById(R.id.cVoltar);
+            Button btnVoltar = findViewById(R.id.aVoltar);
 
             btnVoltar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
 
-                    Intent telaPrincipal = new Intent ( Cadastro.this,MainActivity.class);
+                    Intent telaPrincipal = new Intent ( Alteracao.this,MainActivity.class);
                     startActivity(telaPrincipal);
                 }
             });
@@ -55,20 +54,7 @@ public class Cadastro extends AppCompatActivity {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
             return insets;
-
         });
     }
 }
